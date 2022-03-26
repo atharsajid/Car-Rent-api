3020978e855382e39bcd8b18b9ad931a9091375d
MUHAMMAD ATHAR SAJID <90137896+atharsajid@users.noreply.github.com>
2022-03-26 14:39:33 +0500

    Update views.py

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import ListAPIView
 from rest_framework.generics import CreateAPIView
 from rest_framework.generics import DestroyAPIView
 from rest_framework.generics import UpdateAPIView
-from api.serializers import UsersSerializer
+from api.serializers import UsersSerializer,CarSerializer
 from api.models import CarList, Users
 
 # Create your views here.
@@ -36,22 +36,22 @@ class DeleteUserAPIView(DestroyAPIView):
 class ListCarAPIView(ListAPIView):
     """This endpoint list all of the available todos from the database"""
     queryset = CarList.objects.all()
-    serializer_class = UsersSerializer
+    serializer_class = CarSerializer
 
 
 class CreateCarAPIView(CreateAPIView):
     """This endpoint allows for creation of a todo"""
     queryset = CarList.objects.all()
-    serializer_class = UsersSerializer
+    serializer_class = CarSerializer
 
 
 class UpdateCarAPIView(UpdateAPIView):
     """This endpoint allows for updating a specific todo by passing in the id of the todo to update"""
     queryset = CarList.objects.all()
-    serializer_class = UsersSerializer
+    serializer_class = CarSerializer
 
 
 class DeleteCarAPIView(DestroyAPIView):
     """This endpoint allows for deletion of a specific Todo from the database"""
-    queryset = Users.objects.all()
-    serializer_class = UsersSerializer
+    queryset = .objects.all()
+    serializer_class = CarSerializer
