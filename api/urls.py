67062c2758e0f813cf280004bc69cb368b729354
MUHAMMAD ATHAR SAJID <90137896+atharsajid@users.noreply.github.com>
2022-03-26 11:50:13 +0500

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path("users",views.ListUserAPIView.as_view(),name="user_list"),
+    path("users/",views.ListUserAPIView.as_view(),name="user_list"),
     path("create/", views.CreateUserAPIView.as_view(),name="user_create"),
     path("update/<int:pk>/",views.UpdateUserAPIView.as_view(),name="user_update"),
     path("delete/<int:pk>/",views.DeleteUserAPIView.as_view(),name="user_delete")
